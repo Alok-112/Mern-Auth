aaa049b19ea42a92620c0816b14ac3fa7b21ca23
alok-112 <a.thakurm007@.com>
2024-02-28 19:25:47 +0530

    fix

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -4,9 +4,9 @@ import { UserContext } from "../../context/userContext";
 const Dashboard = () => {
   const { user } = useContext(UserContext);
   return (
-    <div class="flex flex-col items-center justify-center min-h-screen text-2xl ">
-    <h1 class="text-4xl text-violet-600">DashBoard</h1>
-    { !!user && ( <h1 class="text-4xl">Hi {user.name}!</h1> )}
+    <div className="flex flex-col items-center justify-center min-h-screen text-2xl ">
+    <h1 className="text-4xl text-violet-600">DashBoard</h1>
+    { !!user && ( <h1 className="text-4xl">Hi {user.name}!</h1> )}
   </div>
   );
 };
